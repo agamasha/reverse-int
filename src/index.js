079066705b1482292ad2014d047ab46d0ea6a4f6
agamasha <mariaaga17@gmail.com>
2023-08-01 16:24:51 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+  let a = n.toString();
+  for (let i = (a.length - 1); i >= 0; i--){
+    result = result + a[i];
+  }
+  return parseInt(result);
 }
